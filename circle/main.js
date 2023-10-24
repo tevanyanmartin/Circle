7c1e6021e1b2bb57d5e1e7979df4abf4e31cb44a
tevanyanmartin <74008842+tevanyanmartin@users.noreply.github.com>
2023-10-25 01:38:14 +0400

    Add files via upload

---
+++ circle/main.js
@@ -0,0 +1,10 @@
+  const textElement = document.querySelectorAll(".text");
+textElement.forEach(element => {
+  const textElementParent = element.parentNode.offsetWidth;
+    const textLength = element.innerText.split("").length
+const transformedText = element.innerText.split("").map((char, i) => {
+    const rotationDegree = i* 10.3;
+    return `<span style="transform:rotate(${rotationDegree}deg);transform-origin:0px ${textElementParent/2}px ">${char}</span>`;
+  }).join("")
+  element.innerHTML = transformedText;
+        });
